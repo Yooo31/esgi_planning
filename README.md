c6faee97d31d574fb8839a92fb9d024fd18c470c
Yooo31 <mrts.yoan@gmail.com>
2022-12-16 12:30:40 +0100

    Add import

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This Project send on ower discord channel, the weekly planning.
 ## INSTALLATION REQUISE
 
 pip install python-dotenv
+pip install selenium
+pip install discord.py
---
+++ myges.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+import os
+import time
+from dotenv import load_dotenv
